c5970533b7990c3c7ab9560a0bb603a4fd2ed085
langwilson <131865349+langwilson@users.noreply.github.com>
2024-08-28 20:51:38 +1000

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-function toBack() {
-		const pages = document.getElementsByClassName("page");
-		for (let x = 0; x < pages.length; x++) {
-			pages[x].style.zIndex = Number(getComputedStyle(pages[x]).zIndex) + 1;
-			if(getComputedStyle(pages[x]).zIndex > pages.length) {
-				pages[x].classList.add("slideIn");
-				pages[x].addEventListener("animationend", function() {
-					pages[x].classList.remove("slideIn");
-					pages[x].classList.add("slideOut");
-					pages[x].style.zIndex = 1;
-					pages[x].addEventListener("animationend", function() {pages[x].classList.remove("slideOut")}, {once: true});
-				}, {once: true});
-			}
+pages = document.getElementsByClassName("page");
+shift = 0;
+
+function toBack(pageNum) {
+	for(let x = 0; x < pages.length; x++) {
+		pages[x].style.zIndex = Number(getComputedStyle(pages[x]).zIndex) + 1;
 	}
+	pages[pageNum].classList.add("slideIn");  //add the slidein animation to the current page
+	pages[pageNum].addEventListener("animationend", function() {  //add event listener to be called when the animation ends
+		pages[pageNum].classList.remove("slideIn");  //remove the slidein animation
+		pages[pageNum].classList.add("slideOut");  //add the slideout animation
+		pages[pageNum].style.zIndex = 1;
+		pages[pageNum].addEventListener("animationend", function() {pages[pageNum].classList.remove("slideOut")}, {once: true}); //add event listener to remove the animation once it ends
+	}, {once: true});
+}
+
+for (let x = 0; x < pages.length; x++) {
+	pages[x].style.zIndex = x+1;
+
+	pages[x].addEventListener('click', function() {
+		toBack(x);
+	});
 }
